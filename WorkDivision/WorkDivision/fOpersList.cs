f3d08df3c6def58902b633004b78737f37085311
Aleksander <aleksander.kolyada@gmail.com>
2023-12-19 17:00:25 +0300

    Add form for operations selecting by division.

---
+++ WorkDivision/WorkDivision/fOpersList.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+
+namespace WorkDivision
+{
+    public partial class fOpersList : Form
+    {
+        //Поделючение к базе PED
+        SqlConnection connMed;
+        //MedDB meddb = null;
+
+        private SQLiteConnection dblite;
+        private SQLiteDataReader sqlReader;
+        SQLiteCommand m_sqlCmd = null;
+
+        SQLiteCommand updateDirCommand = null;
+        SQLiteCommand insertDirCommand = null;
+
+        public fOpersList()
+        {
+            InitializeComponent();
+            //meddb = new MedDB();        // БД PED
+            //connMed = meddb.GetConnection();
+        }
+
+        //Кнопка Найти
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fAddOutput_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fAddSickList_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            dblite.Close();
+        }
+
+        private void cbFIO_TextUpdate(object sender, EventArgs e)
+        {
+            DataTable DF = new DataTable();
+            DF.Clear();
+
+
+            if (this.cbOperation.Text != "")
+            {
+                string st = cbOperation.Text;
+                cbOperation.Items.Clear();
+                treeView1.Nodes.Clear();
+                //Запрос к базе данных
+                string query = @"SELECT name, id FROM DirOpers WHERE 
+                                        Name like '%" + cbOperation.Text + @"%'";
+                DF = GetMedData(query);
+                if (DF.Rows.Count > 0)
+                {
+                    for (int i = 0; i < DF.Rows.Count; i++)
+                    {
+                        cbOperation.Items.Add(DF.Rows[i].ItemArray[0].ToString());
+                        var node = new TreeNode(DF.Rows[i].ItemArray[0].ToString());
+                        node.Tag = DF.Rows[i].ItemArray[1].ToString();  //id
+                        treeView1.Nodes.Add(node);
+
+                        cbOperation.SelectionStart = cbOperation.Text.Length;
+                    }
+                    cbOperation.SelectionStart = cbOperation.Text.Length;
+                    //cbDeed.DroppedDown = true;
+                    cbOperation.Text = st;
+
+                }
+                else
+                {
+                    cbOperation.Text = st;
+                    cbOperation.SelectionStart = cbOperation.Text.Length;
+                    cbOperation.Items.Clear();
+                    treeView1.Nodes.Clear();
+                }
+            }
+            cbOperation.SelectionStart = cbOperation.Text.Length;
+        }
+
+        public DataTable GetMedData(string query)
+        {
+            //connMed.Open();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                SqlCommand comm = new SqlCommand(query, connMed);
+
+                SqlDataAdapter da = new SqlDataAdapter(comm);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //connMed.Close();
+            }
+
+            return dt;
+        }
+
+        private void cbDeed_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //btnSearch.Select();
+                btnOk_Click(this,null);
+            }
+        }
+
+        private async void getOpersForModel()
+        {
+            string st;
+            treeView1.Nodes.Clear();
+            //TreeNode visitNode = new TreeNode("Консультации и приемы");
+            try
+            {
+                string query = @"SELECT * FROM DirOpers ORDER BY PER";
+                m_sqlCmd = new SQLiteCommand(query, dblite);
+                sqlReader = m_sqlCmd.ExecuteReader();
+                //treeView1.Nodes[1].Nodes.Remove();
+                while (await sqlReader.ReadAsync())
+                {
+                    //MessageBox.Show(st + @"(" + Convert.ToDateTime(sqlMedReader["visit_date"]).ToString("dd.MM.yyyy HH:mm") + @")");
+                    //st = sqlReader["UCH"].ToString() + " " + sqlReader["Name"].ToString();
+                    var node = new TreeNode(sqlReader["Name"].ToString());
+                    node.Tag = sqlReader["id"].ToString();
+                    treeView1.Nodes.Add(node);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+            //treeView1.Nodes.Add(visitNode);
+            //treeView1.Expand(); //Раскрываем приемы
+            //treeView1.SelectedNode = visitNode.LastNode; //Выделяем последний прием
+        }
+
+        private void fDeedsList_Load(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            getOpersForModel();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private async void btnOk_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                foreach (TreeNode opersnode in treeView1.Nodes)
+                {
+                    if (opersnode.Checked)
+                    {
+                        insertDirCommand = new SQLiteCommand(@"INSERT INTO inDivision (id_division,id_oper,mm,yy) 
+                        VALUES (@id_division,@id_oper,@mm,@yy)", dblite);
+                        insertDirCommand.Parameters.AddWithValue("id_division", Division.id);
+                        insertDirCommand.Parameters.AddWithValue("id_oper", Convert.ToInt32(opersnode.Tag));
+                        insertDirCommand.Parameters.AddWithValue("mm",Division.mm);
+                        insertDirCommand.Parameters.AddWithValue("yy",Division.yy);
+                        await insertDirCommand.ExecuteNonQueryAsync();
+                    }
+                }
+
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.21.08", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ WorkDivision/WorkDivision/fOpersList.designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WorkDivision
+{
+    partial class fOpersList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblID = new System.Windows.Forms.Label();
+            this.cbOperation = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // btnOk
+            // 
+            this.btnOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnOk.Location = new System.Drawing.Point(507, 545);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(124, 23);
+            this.btnOk.TabIndex = 1;
+            this.btnOk.Text = "Добавить";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(12, 9);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 13);
+            this.lblID.TabIndex = 2;
+            // 
+            // cbDeed
+            // 
+            this.cbOperation.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cbOperation.FormattingEnabled = true;
+            this.cbOperation.Location = new System.Drawing.Point(15, 25);
+            this.cbOperation.Name = "cbDeed";
+            this.cbOperation.Size = new System.Drawing.Size(725, 21);
+            this.cbOperation.TabIndex = 19;
+            this.cbOperation.TextUpdate += new System.EventHandler(this.cbFIO_TextUpdate);
+            this.cbOperation.KeyDown += new System.Windows.Forms.KeyEventHandler(this.cbDeed_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 13);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Поиск операции";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(647, 545);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Отменить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeView1
+            // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.treeView1.CheckBoxes = true;
+            this.treeView1.Location = new System.Drawing.Point(15, 67);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(725, 455);
+            this.treeView1.TabIndex = 34;
+            // 
+            // fOpersList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 580);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbOperation);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.btnOk);
+            this.Name = "fOpersList";
+            this.Text = "Выбор операций";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.fAddSickList_FormClosed);
+            this.Load += new System.EventHandler(this.fDeedsList_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.ComboBox cbOperation;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
