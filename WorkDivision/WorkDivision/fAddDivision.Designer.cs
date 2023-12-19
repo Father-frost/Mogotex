f9cfd1062b0abc17b7015190f6219e41b46a2a74
Aleksander <aleksander.kolyada@gmail.com>
2023-12-19 16:59:32 +0300

    Add work division list view and form for divisions editing.

---
+++ WorkDivision/WorkDivision/fAddDivision.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace WorkDivision
+{
+    partial class fAddDivision
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbProd = new System.Windows.Forms.TextBox();
+            this.tbEI = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbModel = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbProdCat = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbProdGRP = new System.Windows.Forms.TextBox();
+            this.lbKMOD = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(31, 23);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 16);
+            this.lblID.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 240);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 28);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Наименование модели:";
+            // 
+            // tbProd
+            // 
+            this.tbProd.Location = new System.Drawing.Point(34, 134);
+            this.tbProd.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbProd.Name = "tbProd";
+            this.tbProd.Size = new System.Drawing.Size(215, 22);
+            this.tbProd.TabIndex = 25;
+            // 
+            // tbEI
+            // 
+            this.tbEI.Location = new System.Drawing.Point(34, 192);
+            this.tbEI.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tbEI.Name = "tbEI";
+            this.tbEI.Size = new System.Drawing.Size(215, 22);
+            this.tbEI.TabIndex = 32;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 172);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 16);
+            this.label6.TabIndex = 31;
+            this.label6.Text = "Ед. изм.:";
+            // 
+            // cbModel
+            // 
+            this.cbModel.FormattingEnabled = true;
+            this.cbModel.Location = new System.Drawing.Point(34, 80);
+            this.cbModel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cbModel.Name = "cbModel";
+            this.cbModel.Size = new System.Drawing.Size(748, 24);
+            this.cbModel.TabIndex = 34;
+            this.cbModel.TextUpdate += new System.EventHandler(this.cbModel_TextUpdate);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 114);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 33;
+            this.label3.Text = "Вид изделия:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 114);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Категория 1:";
+            // 
+            // tbProdCat
+            // 
+            this.tbProdCat.Location = new System.Drawing.Point(289, 134);
+            this.tbProdCat.Margin = new System.Windows.Forms.Padding(4);
+            this.tbProdCat.Name = "tbProdCat";
+            this.tbProdCat.Size = new System.Drawing.Size(238, 22);
+            this.tbProdCat.TabIndex = 40;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(553, 114);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 16);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "Категория 2:";
+            // 
+            // tbProdGRP
+            // 
+            this.tbProdGRP.Location = new System.Drawing.Point(556, 134);
+            this.tbProdGRP.Margin = new System.Windows.Forms.Padding(4);
+            this.tbProdGRP.Name = "tbProdGRP";
+            this.tbProdGRP.Size = new System.Drawing.Size(226, 22);
+            this.tbProdGRP.TabIndex = 42;
+            // 
+            // lbKMOD
+            // 
+            this.lbKMOD.AutoSize = true;
+            this.lbKMOD.Location = new System.Drawing.Point(31, 50);
+            this.lbKMOD.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbKMOD.Name = "lbKMOD";
+            this.lbKMOD.Size = new System.Drawing.Size(0, 16);
+            this.lbKMOD.TabIndex = 44;
+            // 
+            // fAddDivision
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 300);
+            this.Controls.Add(this.lbKMOD);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbProdGRP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbProdCat);
+            this.Controls.Add(this.cbModel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbEI);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.tbProd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "fAddDivision";
+            this.Text = "Добавить / Изменить разделение";
+            this.Load += new System.EventHandler(this.fAddModel_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.fAddDivision_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbProd;
+        private System.Windows.Forms.TextBox tbEI;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbModel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbProdCat;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbProdGRP;
+        private System.Windows.Forms.Label lbKMOD;
+    }
+}
---
+++ WorkDivision/WorkDivision/fAddDivision.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Xml;
+using static System.Net.WebRequestMethods;
+using System.Collections;
+using System.Data.SqlClient;
+
+namespace WorkDivision
+{
+    public partial class fAddDivision : Form
+    {
+        private SQLiteConnection dblite;
+        private SQLiteDataReader sqlReader;
+        SQLiteCommand m_sqlCmd = null;
+        
+
+        public fAddDivision()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            if (id_rec == "")  //Если  id записи не передан (новая запись) 
+            {
+                
+                string querySQLite = @"INSERT INTO Division (id_model) VALUES (@id_model)";
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@id_model", cbModel.SelectedValue);
+            }
+            else
+            {
+                string querySQLite = @"UPDATE Division SET  id_model=@id_model WHERE id=" + id_rec;
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@id_model", cbModel.SelectedValue);
+            }
+            try
+            {
+                m_sqlCmd.Prepare();
+                m_sqlCmd.ExecuteNonQuery();
+                this.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.21.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public string id_rec { get; set; }
+
+        private void fAddModel_Load(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            string query = @"SELECT id,Name FROM DirModels ORDER by Name";
+            DataTable dt = new DataTable();
+
+            try
+            {
+                SQLiteCommand m_sqlCmd = new SQLiteCommand(query, dblite);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(m_sqlCmd);
+                DataSet ds = new DataSet();
+
+                da.Fill(ds);
+                dt = ds.Tables[0];
+
+                cbModel.Select();
+                cbModel.DataSource = dt.AsDataView();
+                cbModel.DisplayMember = "Name";
+                cbModel.ValueMember = "id";
+
+                //Если выбран существующий интервал, загружаем параметры
+                if (id_rec != "")
+                {
+                    //MessageBox.Show(id_rec.ToString());
+                    GetDivision();
+                }
+                else
+                {
+                    cbModel.Text = "";
+                    tbProd.Text = "";
+                    tbProdCat.Text = "";
+                    tbProdGRP.Text = "";
+                    tbEI.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.21.03", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+        }
+
+        public async void GetDivision()
+        {
+            try
+            {
+                //dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+                //dblite.Open();
+                string query = @"SELECT dm.Name,dm.KMOD,dm.EI,dp.Name as pName,dpc.category, dpg.GRP FROM Division as div
+                               LEFT JOIN DirModels as dm on div.id_model=dm.id 
+                               LEFT JOIN DirProducts as dp on dp.id=dm.id_product
+                               LEFT JOIN DirProdCat as dpc on dpc.id=dm.id_cat 
+                               LEFT JOIN DirProdGRP as dpg on dpg.id=dm.id_grp WHERE div.id =" + id_rec;
+
+                m_sqlCmd = new SQLiteCommand(query, dblite);
+                //m_sqlCmd.Connection = dblite;
+
+                sqlReader = m_sqlCmd.ExecuteReader();
+
+                while (await sqlReader.ReadAsync())
+                {
+                    cbModel.Text = Convert.ToString(sqlReader["Name"]);
+                    lbKMOD.Text = Convert.ToString(sqlReader["KMOD"]);
+                    tbEI.Text = Convert.ToString(sqlReader["EI"]);
+                    tbProd.Text = Convert.ToString(sqlReader["pName"]);
+                    tbProdCat.Text = Convert.ToString(sqlReader["category"]);
+                    tbProdGRP.Text = Convert.ToString(sqlReader["GRP"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.06.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+        }
+
+        private void cbModel_TextUpdate(object sender, EventArgs e)
+        {
+            DataTable DF = new DataTable();
+            DF.Clear();
+
+
+            if (this.cbModel.Text != "")
+            {
+                string st = cbModel.Text;
+                cbModel.Items.Clear();
+                //Запрос к базе данных
+                string query = @"SELECT Name FROM DirModels WHERE 
+                                        Name like '%" + cbModel.Text + @"%'";
+                DF = GetMedData(query);
+                if (DF.Rows.Count > 0)
+                {
+                    for (int i = 0; i < DF.Rows.Count; i++)
+                    {
+                        cbModel.Items.Add(DF.Rows[i].ItemArray[0].ToString());
+
+                        cbModel.SelectionStart = cbModel.Text.Length;
+                    }
+                    cbModel.SelectionStart = cbModel.Text.Length;
+                    cbModel.DroppedDown = true;
+                    cbModel.Text = st;
+
+                }
+                else
+                {
+                    cbModel.Text = st;
+                    cbModel.SelectionStart = cbModel.Text.Length;
+                    cbModel.Items.Clear();
+                }
+            }
+            cbModel.SelectionStart = cbModel.Text.Length;
+        }
+
+        public DataTable GetMedData(string query)
+        {
+            //connMed.Open();
+            DataTable dt = new DataTable();
+
+            try
+            {
+                SQLiteCommand comm = new SQLiteCommand(query, dblite);
+
+                SQLiteDataAdapter da = new SQLiteDataAdapter(comm);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                dt = ds.Tables[0];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                //connMed.Close();
+            }
+
+            return dt;
+        }
+
+        private void fAddDivision_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                //btnSearch.Select();
+                //btnSearch_Click(this, null);
+            }
+        }
+    }
+}
