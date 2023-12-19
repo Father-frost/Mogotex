16f5e0485143c47477ce451e9013d26df1477785
Aleksander <aleksander.kolyada@gmail.com>
2023-12-19 16:44:10 +0300

    Add Brigades directory and form to edit it.

---
+++ WorkDivision/WorkDivision/fAddBrig.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WorkDivision
+{
+    partial class fAddBrig
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbKODBR = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbNumk = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(35, 36);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 16);
+            this.lblID.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 149);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 28);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(116, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Номер:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 71);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Название бригады:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 101);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 16);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "NUMK:";
+            // 
+            // tbKODBR
+            // 
+            this.tbKODBR.Location = new System.Drawing.Point(181, 36);
+            this.tbKODBR.Margin = new System.Windows.Forms.Padding(4);
+            this.tbKODBR.Name = "tbKODBR";
+            this.tbKODBR.Size = new System.Drawing.Size(96, 22);
+            this.tbKODBR.TabIndex = 24;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(182, 68);
+            this.tbName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(397, 22);
+            this.tbName.TabIndex = 25;
+            // 
+            // tbNumk
+            // 
+            this.tbNumk.Location = new System.Drawing.Point(181, 101);
+            this.tbNumk.Margin = new System.Windows.Forms.Padding(4);
+            this.tbNumk.Name = "tbNumk";
+            this.tbNumk.Size = new System.Drawing.Size(96, 22);
+            this.tbNumk.TabIndex = 26;
+            // 
+            // fAddBrig
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 205);
+            this.Controls.Add(this.tbNumk);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbKODBR);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fAddBrig";
+            this.Text = "Добавить / Изменить запись";
+            this.Load += new System.EventHandler(this.fAddBrig_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbKODBR;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbNumk;
+    }
+}
---
+++ WorkDivision/WorkDivision/fAddBrig.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Xml;
+
+namespace WorkDivision
+{
+    public partial class fAddBrig : Form
+    {
+        private SQLiteConnection dblite;
+        private SQLiteDataReader sqlReader;
+        SQLiteCommand m_sqlCmd = null;
+        
+
+        public fAddBrig()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            if (id_rec == "")  //Если  id записи не передан (новая запись) 
+            {
+                string querySQLite = @"INSERT INTO DirBrigs (KODBR, Name, Numk) VALUES (@KODBR, @Name, @Numk)";
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@KODBR", tbKODBR.Text);
+                m_sqlCmd.Parameters.AddWithValue("@Name", tbName.Text);
+                m_sqlCmd.Parameters.AddWithValue("@Numk", tbNumk.Text);
+            }
+            else
+            {
+                string querySQLite = @"UPDATE DirBrigs SET KODBR=@KODBR, Name=@Name, Numk=@Numk WHERE id=" + id_rec;
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@KODBR", tbKODBR.Text);
+                m_sqlCmd.Parameters.AddWithValue("@Name", tbName.Text);
+                m_sqlCmd.Parameters.AddWithValue("@Numk", tbNumk.Text);
+            }
+            try
+            {
+                m_sqlCmd.Prepare();
+                m_sqlCmd.ExecuteNonQuery();
+                this.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.21.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public string id_rec { get; set; }
+
+        private void fAddBrig_Load(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            //Если выбран существующий интервал, загружаем параметры
+            if (id_rec != "")
+                {
+                    //MessageBox.Show(id_rec.ToString());
+                    GetBrig();
+                }
+                else
+                {
+                    tbKODBR.Text = "";
+                    tbName.Text = "";
+                    tbNumk.Text = "";
+                }
+        }
+
+        public async void GetBrig()
+        {
+            try
+            {
+                //dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+                //dblite.Open();
+                string query = @"SELECT * FROM DirBrigs WHERE id =" + id_rec;
+
+                m_sqlCmd = new SQLiteCommand(query, dblite);
+                //m_sqlCmd.Connection = dblite;
+
+                sqlReader = m_sqlCmd.ExecuteReader();
+
+                while (await sqlReader.ReadAsync())
+                {
+
+                    tbKODBR.Text = Convert.ToString(sqlReader["KODBR"]);
+                    tbName.Text = Convert.ToString(sqlReader["Name"]);
+                    tbNumk.Text = Convert.ToString(sqlReader["Numk"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.01.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+        }
+
+    }
+}
