2871e8f5ba3db1eb9125be17c548d1673b108944
Aleksander <aleksander.kolyada@gmail.com>
2023-12-19 16:43:11 +0300

    Add Workers directory and form to edit it.

---
+++ WorkDivision/WorkDivision/fAddWorker.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace WorkDivision
+{
+    partial class fAddWorker
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbBrig = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbTabnom = new System.Windows.Forms.TextBox();
+            this.tbFIO = new System.Windows.Forms.TextBox();
+            this.tbRank = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbKO = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbProf = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cbBrig
+            // 
+            this.cbBrig.FormattingEnabled = true;
+            this.cbBrig.Location = new System.Drawing.Point(160, 168);
+            this.cbBrig.Margin = new System.Windows.Forms.Padding(4);
+            this.cbBrig.Name = "cbBrig";
+            this.cbBrig.Size = new System.Drawing.Size(419, 24);
+            this.cbBrig.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(78, 171);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 16);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Бригада:";
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(35, 36);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 16);
+            this.lblID.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 258);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 28);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 36);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Таб. номер:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "ФИО:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(85, 101);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 16);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Разряд:";
+            // 
+            // tbTabnom
+            // 
+            this.tbTabnom.Location = new System.Drawing.Point(160, 36);
+            this.tbTabnom.Margin = new System.Windows.Forms.Padding(4);
+            this.tbTabnom.Name = "tbTabnom";
+            this.tbTabnom.Size = new System.Drawing.Size(96, 22);
+            this.tbTabnom.TabIndex = 24;
+            // 
+            // tbFIO
+            // 
+            this.tbFIO.Location = new System.Drawing.Point(160, 68);
+            this.tbFIO.Margin = new System.Windows.Forms.Padding(4);
+            this.tbFIO.Name = "tbFIO";
+            this.tbFIO.Size = new System.Drawing.Size(419, 22);
+            this.tbFIO.TabIndex = 25;
+            // 
+            // tbRank
+            // 
+            this.tbRank.Location = new System.Drawing.Point(160, 101);
+            this.tbRank.Margin = new System.Windows.Forms.Padding(4);
+            this.tbRank.Name = "tbRank";
+            this.tbRank.Size = new System.Drawing.Size(96, 22);
+            this.tbRank.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 211);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Профессия:";
+            // 
+            // tbKO
+            // 
+            this.tbKO.Location = new System.Drawing.Point(160, 134);
+            this.tbKO.Margin = new System.Windows.Forms.Padding(4);
+            this.tbKO.Name = "tbKO";
+            this.tbKO.Size = new System.Drawing.Size(96, 22);
+            this.tbKO.TabIndex = 30;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(115, 137);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 16);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "KO:";
+            // 
+            // cbProf
+            // 
+            this.cbProf.FormattingEnabled = true;
+            this.cbProf.Location = new System.Drawing.Point(160, 208);
+            this.cbProf.Margin = new System.Windows.Forms.Padding(4);
+            this.cbProf.Name = "cbProf";
+            this.cbProf.Size = new System.Drawing.Size(419, 24);
+            this.cbProf.TabIndex = 31;
+            // 
+            // fAddWorker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 311);
+            this.Controls.Add(this.cbProf);
+            this.Controls.Add(this.tbKO);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.tbRank);
+            this.Controls.Add(this.tbFIO);
+            this.Controls.Add(this.tbTabnom);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbBrig);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fAddWorker";
+            this.Text = "Добавить / Изменить запись";
+            this.Load += new System.EventHandler(this.fAddWorker_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbBrig;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbTabnom;
+        private System.Windows.Forms.TextBox tbFIO;
+        private System.Windows.Forms.TextBox tbRank;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbKO;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbProf;
+    }
+}
---
+++ WorkDivision/WorkDivision/fAddWorker.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Xml;
+
+namespace WorkDivision
+{
+    public partial class fAddWorker : Form
+    {
+        private SQLiteConnection dblite;
+        private SQLiteDataReader sqlReader;
+        SQLiteCommand m_sqlCmd = null;
+        
+
+        public fAddWorker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            if (id_rec == "")  //Если  id записи не передан (новая запись) 
+            {
+                string querySQLite = @"INSERT INTO DirWorkers (Tab_nom, FIO, rank, kprof, KO, brig_id) VALUES (@Tab_nom,@FIO,@rank,@kprof,@KO,@brig_id)";
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@tab_nom", tbTabnom.Text);
+                m_sqlCmd.Parameters.AddWithValue("@FIO", tbFIO.Text);
+                m_sqlCmd.Parameters.AddWithValue("@rank", tbRank.Text);
+                m_sqlCmd.Parameters.AddWithValue("@kprof", cbProf.SelectedValue);
+                m_sqlCmd.Parameters.AddWithValue("@KO", tbKO.Text.Replace(",", "."));
+                m_sqlCmd.Parameters.AddWithValue("@brig_id", cbBrig.SelectedValue);
+            }
+            else
+            {
+                string querySQLite = @"UPDATE DirWorkers SET Tab_nom=@Tab_nom, FIO=@FIO, rank=@rank, kprof=@kprof, KO=@KO, brig_id=@brig_id WHERE id="+id_rec;
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@tab_nom", tbTabnom.Text);
+                m_sqlCmd.Parameters.AddWithValue("@FIO", tbFIO.Text);
+                m_sqlCmd.Parameters.AddWithValue("@rank", tbRank.Text);
+                m_sqlCmd.Parameters.AddWithValue("@kprof", cbProf.SelectedValue);
+                m_sqlCmd.Parameters.AddWithValue("@KO", tbKO.Text.Replace(",", "."));
+                m_sqlCmd.Parameters.AddWithValue("@brig_id", cbBrig.SelectedValue);
+            }
+            try
+            {
+                m_sqlCmd.Prepare();
+                m_sqlCmd.ExecuteNonQuery();
+                this.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.21.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public string id_rec { get; set; }
+
+        private void fAddWorker_Load(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            DataTable dt = new DataTable();
+            DataTable dtp = new DataTable();
+            string query = @"SELECT id,(KODBR ||' '|| Name) as lName FROM DirBrigs";
+            string query1 = @"SELECT id,Name as pName FROM DirProfs";
+
+            try
+            {
+                SQLiteCommand m_sqlCmd = new SQLiteCommand(query, dblite);
+                SQLiteDataAdapter da = new SQLiteDataAdapter(m_sqlCmd);
+                DataSet ds = new DataSet();
+
+                da.Fill(ds);
+                dt = ds.Tables[0];
+
+                m_sqlCmd = new SQLiteCommand(query1, dblite);
+                da = new SQLiteDataAdapter(m_sqlCmd);
+
+                DataSet ds1 = new DataSet();
+                da.Fill(ds1);
+                dtp = ds1.Tables[0];
+
+                cbBrig.DataSource = dt.AsDataView();
+                cbBrig.DisplayMember = "lName";
+                cbBrig.ValueMember = "id";
+                
+                cbProf.DataSource = dtp.AsDataView();
+                cbProf.DisplayMember = "pName";
+                cbProf.ValueMember = "id";
+
+                //Если выбран существующий интервал, загружаем параметры
+                if (id_rec != "")
+                {
+                    //MessageBox.Show(id_rec.ToString());
+                    GetWorker();
+                }
+                else
+                {
+                    tbTabnom.Text = "";
+                    tbFIO.Text = "";
+                    tbRank.Text = "";
+                    tbKO.Text = "";
+                }
+
+
+
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+
+        }
+
+        public async void GetWorker()
+        {
+            try
+            {
+                dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+                dblite.Open();
+                string query = @"SELECT dw.*,dbr.id,dbr.KODBR,dbr.Name,pr.Name as pName 
+                            FROM DirWorkers as dw
+                            LEFT JOIN DirProfs as pr on pr.id = dw.kprof
+                            LEFT JOIN DirBrigs as dbr on dw.brig_id = dbr.id WHERE dw.id =" + id_rec;
+
+                m_sqlCmd = new SQLiteCommand(query, dblite);
+                //m_sqlCmd.Connection = dblite;
+
+                sqlReader = m_sqlCmd.ExecuteReader();
+
+                while (await sqlReader.ReadAsync())
+                {
+
+                    tbTabnom.Text = Convert.ToString(sqlReader["Tab_nom"]);
+                    tbFIO.Text = Convert.ToString(sqlReader["FIO"]);
+                    tbRank.Text = Convert.ToString(sqlReader["rank"]);
+                    cbProf.Text =  Convert.ToString(sqlReader["pName"]);
+                    tbKO.Text =  Convert.ToString(sqlReader["KO"]);
+                    cbBrig.Text = Convert.ToString(sqlReader["KODBR"])+" "+Convert.ToString(sqlReader["Name"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.00.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+        }
+
+    }
+}
