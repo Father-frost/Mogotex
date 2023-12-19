9f28a4817342c5ce240cd91fc5eaebe3d1532b71
Aleksander <aleksander.kolyada@gmail.com>
2023-12-19 16:47:06 +0300

    Add Products directory and form to edit it.

---
+++ WorkDivision/WorkDivision/fAddProd.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WorkDivision
+{
+    partial class fAddProd
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(26, 29);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 13);
+            this.lblID.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(247, 71);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(185, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Наименование:";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(101, 29);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(331, 20);
+            this.tbName.TabIndex = 25;
+            // 
+            // fAddProd
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 122);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button1);
+            this.Name = "fAddProd";
+            this.Text = "Добавить / Изменить запись";
+            this.Load += new System.EventHandler(this.fAddProd_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbName;
+    }
+}
---
+++ WorkDivision/WorkDivision/fAddProd.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using System.Xml;
+
+namespace WorkDivision
+{
+    public partial class fAddProd : Form
+    {
+        private SQLiteConnection dblite;
+        private SQLiteDataReader sqlReader;
+        SQLiteCommand m_sqlCmd = null;
+        
+
+        public fAddProd()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            if (id_rec == "")  //Если  id записи не передан (новая запись) 
+            {
+                string querySQLite = @"INSERT INTO DirProducts (Name) VALUES (@Name)";
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@Name", tbName.Text);
+            }
+            else
+            {
+                string querySQLite = @"UPDATE DirProducts SET Name=@Name WHERE id=" + id_rec;
+                m_sqlCmd = new SQLiteCommand(querySQLite, dblite);
+                m_sqlCmd.Parameters.AddWithValue("@Name", tbName.Text);
+            }
+            try
+            {
+                m_sqlCmd.Prepare();
+                m_sqlCmd.ExecuteNonQuery();
+                this.Close();
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.21.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public string id_rec { get; set; }
+
+        private void fAddProd_Load(object sender, EventArgs e)
+        {
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+            //Если выбран существующий интервал, загружаем параметры
+            if (id_rec != "")
+                {
+                    //MessageBox.Show(id_rec.ToString());
+                    GetProd();
+                }
+                else
+                {
+                    tbName.Text = "";
+                }
+        }
+
+        public async void GetProd()
+        {
+            try
+            {
+                //dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+                //dblite.Open();
+                string query = @"SELECT * FROM DirProducts WHERE id =" + id_rec;
+
+                m_sqlCmd = new SQLiteCommand(query, dblite);
+                //m_sqlCmd.Connection = dblite;
+
+                sqlReader = m_sqlCmd.ExecuteReader();
+
+                while (await sqlReader.ReadAsync())
+                {
+                    tbName.Text = Convert.ToString(sqlReader["Name"]);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.03.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+        }
+
+    }
+}
