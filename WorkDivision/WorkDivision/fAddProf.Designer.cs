b04704452490a93b3a7315a461438ccaceb178d2
Aleksander <aleksander.kolyada@gmail.com>
2023-12-19 16:46:14 +0300

    Add Professions directory and form to edit it.

---
+++ WorkDivision/WorkDivision/fAddProf.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace WorkDivision
+{
+    partial class fAddProf
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbKprof = new System.Windows.Forms.TextBox();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbPR = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(35, 36);
+            this.lblID.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(0, 16);
+            this.lblID.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 149);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 28);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Сохранить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Код профессии:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 68);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Название:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(136, 104);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 16);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "PR:";
+            // 
+            // tbKprof
+            // 
+            this.tbKprof.Location = new System.Drawing.Point(181, 36);
+            this.tbKprof.Margin = new System.Windows.Forms.Padding(4);
+            this.tbKprof.Name = "tbKprof";
+            this.tbKprof.Size = new System.Drawing.Size(96, 22);
+            this.tbKprof.TabIndex = 24;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(182, 68);
+            this.tbName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(397, 22);
+            this.tbName.TabIndex = 25;
+            // 
+            // tbPR
+            // 
+            this.tbPR.Location = new System.Drawing.Point(181, 101);
+            this.tbPR.Margin = new System.Windows.Forms.Padding(4);
+            this.tbPR.Name = "tbPR";
+            this.tbPR.Size = new System.Drawing.Size(96, 22);
+            this.tbPR.TabIndex = 26;
+            // 
+            // fAddProf
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 205);
+            this.Controls.Add(this.tbPR);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbKprof);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fAddProf";
+            this.Text = "Добавить / Изменить запись";
+            this.Load += new System.EventHandler(this.fAddProf_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tbKprof;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbPR;
+    }
+}
