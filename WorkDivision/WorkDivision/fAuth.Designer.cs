8353bafc3b0d7a5a3ea756e485b593b44b9b9f47
Александр Коляда <aleksander.kolyada@gmail.com>
2023-12-21 23:19:56 +0300

    Add authentification.  It disabled now.

---
+++ WorkDivision/WorkDivision/fAuth.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace WorkDivision
+{
+    partial class fAuth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fAuth));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9.5F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(74, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 19);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Распределение труда";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9.5F);
+            this.label3.Location = new System.Drawing.Point(68, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Пароль:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9.5F);
+            this.label2.Location = new System.Drawing.Point(68, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 18);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Пользователь:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(15, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.5F);
+            this.textBox1.Location = new System.Drawing.Point(231, 88);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(200, 26);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9.5F);
+            this.button2.Location = new System.Drawing.Point(330, 134);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 27);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Отмена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.5F);
+            this.button1.Location = new System.Drawing.Point(230, 134);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 27);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Arial", 9.5F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(231, 56);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 26);
+            this.comboBox1.TabIndex = 14;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // fAuth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 173);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fAuth";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.fAuth_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
---
+++ WorkDivision/WorkDivision/fAuth.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkDivision
+{
+    public partial class fAuth : Form
+    {
+        public Form1 f1;
+        private String dbFileName;
+        private SQLiteConnection dblite;
+        private SQLiteCommand m_sqlCmd;
+        private SQLiteDataReader sqlReader;
+        public fAuth()
+        {
+            InitializeComponent();
+            f1 = new Form1();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Authentication();
+        }
+
+        private void fAuth_Load(object sender, EventArgs e)
+        {
+
+            dblite = new SQLiteConnection("Data Source=divisionDB.db;Version=3;");
+            dblite.Open();
+
+            textBox1.Focus();
+
+            // загрузка списка Пользователей в список comboBox1
+            SQLiteCommand getfio = new SQLiteCommand("SELECT * FROM Users ORDER by IdGroup", dblite);
+
+            sqlReader = null;
+
+            try
+            {
+
+                sqlReader = getfio.ExecuteReader();
+
+                while (sqlReader.Read())
+                {
+                    comboBox1.Items.Add(sqlReader["Client"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 1.01", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+
+            // загрузка последнего входящего пользователя
+            //SqlCommand LoadInput;
+            //SqlCommand LoadClient;
+            //LoadInput = new SqlCommand("SELECT LastInput FROM Auth WHERE LastInput = 1", connUacc);
+            //LoadClient = new SqlCommand("SELECT Client FROM Auth WHERE LastInput = 1", connUacc);
+
+            comboBox1.Text = Convert.ToString(comboBox1.Items[0]);
+            /*
+            // получение ФИО пользователя
+            int LastPerson = Convert.ToInt32(LoadInput.ExecuteScalar());
+            if (LastPerson == 1)
+            {
+                comboBox1.Text = Convert.ToString(LoadClient.ExecuteScalar());
+            }
+            else
+            {
+                comboBox1.Text = Convert.ToString(comboBox1.Items[0]);
+            }     
+            */
+        }
+
+        public void Authentication()
+        {
+            SQLiteCommand getfio;
+
+            sqlReader = null;
+
+            getfio = new SQLiteCommand("SELECT * FROM Users", dblite);
+
+            // узнать количество записей
+            //SqlCommand getcount;
+            //getcount = new SqlCommand("SELECT COUNT(*) FROM Auth", connUacc);
+            //iCount = Convert.ToInt32(getcount.ExecuteScalar());
+
+            // очищение параметра временного хранения
+            // статуса последнего пользователя LastInput
+            //SqlCommand UPnullInput;
+            //UPnullInput = new SqlCommand("UPDATE Auth SET LastInput = 0", connUacc);
+            //UPnullInput.ExecuteScalar();
+
+            // добавление параметра временного хранения
+            // статуса последнего пользователя LastInput
+            //SqlCommand UPOut;
+            //UPOut = new SqlCommand("UPDATE Auth SET LastInput = 1  WHERE Client ='" + comboBox1.Text + "'", connUacc);
+
+            if (comboBox1.Text != "")
+            {
+                if (textBox1.Text != "")
+                {
+                    try
+                    {
+                        sqlReader = getfio.ExecuteReader();
+
+                        while (sqlReader.Read())
+                        {
+                            string cl1 = Convert.ToString(sqlReader["Client"]);
+
+                            string rs1 = Convert.ToString(sqlReader["Pass"]);
+
+                            if (Convert.ToString(sqlReader["Client"]) == Convert.ToString(comboBox1.Text))
+                            {
+                                if (Convert.ToString(sqlReader["Pass"]) == textBox1.Text)
+                                {
+                                    Globals.pAuth = Convert.ToString(sqlReader["Client"]);
+                                    Globals.Group = Convert.ToString(sqlReader["Group"]);
+
+                                    //Globals.idGroup = Convert.ToString(sqlReader["IdGroup"]);
+
+                                    if (Convert.ToInt32(sqlReader["IdGroup"]) == 1)
+                                    {
+                                        // Форма пользователя группы доступа 1 - Администратор
+                                        f1.StartPosition = FormStartPosition.CenterParent;
+                                        this.Hide();
+                                        dblite.Close();
+                                        f1.ShowDialog();
+                                    }
+                                    else if (Convert.ToInt32(sqlReader["IdGroup"]) == 2)
+                                    {
+                                        //MessageBox.Show("Мастер");
+                                        // Форма пользователя группы доступа 2 - Мастер цеха
+                                        f1.StartPosition = FormStartPosition.CenterParent;
+                                        this.Hide();
+                                        //dblite.Close();
+                                        f1.ShowDialog();
+                                    }
+                                    else if (Convert.ToInt32(sqlReader["IdGroup"]) == 3)
+                                    {
+                                        // Форма пользователя группы доступа 3 - Пользователь
+                                        f1.StartPosition = FormStartPosition.CenterParent;
+                                        this.Hide();
+                                        //dblite.Close();
+                                        f1.ShowDialog();
+                                    }
+                                    else
+                                        MessageBox.Show("Нет прав доступа!\nОбратитесь к администратору.", "Ошибка 1.02", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                                else if (Convert.ToString(sqlReader["Pass"]) != textBox1.Text)
+                                {
+                                    MessageBox.Show("Пароль не верен!", "Авторизация", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    textBox1.Text = "";
+                                    textBox1.Focus();
+                                    break;
+                                }
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка 1.03", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    finally
+                    {
+                        if (sqlReader != null && !sqlReader.IsClosed)
+                            sqlReader.Close();
+                    }
+                }
+                else
+                    MessageBox.Show("Не правильно введён пароль!", "Авторизация", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("Выберите пользователя!", "Авторизация", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            //  запись в таблицу
+            //  UPOut.ExecuteScalar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Authentication();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Focus();
+        }
+    }
+
+    class Globals
+    {
+        public static string pAuth { get; set; }
+        public static string Group { get; set; }
+        public static string idGroup { get; set; }
+    }
+}
