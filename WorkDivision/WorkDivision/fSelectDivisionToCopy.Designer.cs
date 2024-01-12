e941452ac651fc388faa4e3220f84b46bb733bb4
Aleksander <aleksander.kolyada@gmail.com>
2024-01-12 22:20:00 +0300

    Add functionality for creating new division by copying other one. Add form for selecting division.

---
+++ WorkDivision/WorkDivision/fSelectDivisionToCopy.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WorkDivision
+{
+    partial class fSelectDivisionToCopy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvDivisionsToCopy = new System.Windows.Forms.ListView();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lvDivisionsToCopy
+            // 
+            this.lvDivisionsToCopy.HideSelection = false;
+            this.lvDivisionsToCopy.Location = new System.Drawing.Point(2, 2);
+            this.lvDivisionsToCopy.Name = "lvDivisionsToCopy";
+            this.lvDivisionsToCopy.Size = new System.Drawing.Size(582, 547);
+            this.lvDivisionsToCopy.TabIndex = 0;
+            this.lvDivisionsToCopy.UseCompatibleStateImageBehavior = false;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(191, 555);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(206, 37);
+            this.btnSelect.TabIndex = 1;
+            this.btnSelect.Text = "Выбрать";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // fSelectDivisionToCopy
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 604);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.lvDivisionsToCopy);
+            this.Name = "fSelectDivisionToCopy";
+            this.Text = "Скопировать разделение";
+            this.Load += new System.EventHandler(this.fSelectDivisionToCopy_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvDivisionsToCopy;
+        private System.Windows.Forms.Button btnSelect;
+    }
+}
---
+++ WorkDivision/WorkDivision/fSelectDivisionToCopy.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Data.SQLite;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WorkDivision
+{
+    public partial class fSelectDivisionToCopy : Form
+    {
+        private SQLiteConnection dblite;
+        private SQLiteCommand m_sqlCmd, insertDirCommand;
+        private SQLiteDataReader sqlReader;
+        private liteDB liteDB;
+
+        public fSelectDivisionToCopy()
+        {
+            InitializeComponent();
+            liteDB = new liteDB();
+        }
+
+        private void fSelectDivisionToCopy_Load(object sender, EventArgs e)
+        {
+            //Подключение к БД
+            dblite = liteDB.GetConn();
+            dblite.Open();
+
+            lvDivisionsToCopy.GridLines = true;
+            lvDivisionsToCopy.FullRowSelect = true;
+            lvDivisionsToCopy.View = View.Details;
+            lvDivisionsToCopy.Font = new Font(lvDivisionsToCopy.Font, FontStyle.Bold);
+            lvDivisionsToCopy.Columns.Add("id");
+            lvDivisionsToCopy.Columns.Add("Месяц");
+            lvDivisionsToCopy.Columns.Add("Год");
+            lvDivisionsToCopy.Columns.Add("Модель");
+            lvDivisionsToCopy.Columns.Add("Вид изделия");
+            lvDivisionsToCopy.Columns.Add("Продолжительность");
+            lvDivisionsToCopy.Columns.Add("Стоимость");
+
+            LoadDivisionsForCopy(Division.id);
+            Division.autoResizeColumns(lvDivisionsToCopy);
+        }
+
+        private async void LoadDivisionsForCopy(string without_id)
+        {
+            lvDivisionsToCopy.Items.Clear();  //Чистим listview
+
+            try
+            {
+                string query = @"SELECT div.id,div.id_model,dm.name as mName,
+                    dp.Name as pName, dc.category, dg.GRP,
+                    (SELECT SUM(CASE WHEN d.UCH between 1 and 2 THEN ifnull(ROUND(i.NVRforOper * i.MatRate * i.workers_cnt,2),0) ELSE NVRforOper END)
+                    FROM inDivision as i LEFT JOIN DirOpers as d on d.id=i.id_oper LEFT JOIN DirTarif as dt on dt.rank=i.rank
+                    WHERE id_division=div.id) as work_time,
+                    (SELECT SUM(CASE WHEN d.UCH between 1 and 2 THEN ifnull(ROUND(ROUND(i.NVRforOper * dt.TAR_VR,5) * i.MatRate * i.workers_cnt,5),0) 
+                    ELSE ifnull(ROUND(i.NVRforOper * dt.TAR_VR * i.workers_cnt,5),0) END)
+                    FROM inDivision as i LEFT JOIN DirOpers as d on d.id=i.id_oper LEFT JOIN DirTarif as dt on dt.rank=i.rank
+                    WHERE id_division=div.id) as cost, div.mm, div.yy
+                    FROM Division as div
+                    LEFT JOIN DirModels as dm on div.id_model=dm.id 
+                    LEFT JOIN DirProducts as dp on dm.id_product=dp.id
+                    LEFT JOIN DirProdCat as dc on dm.id_cat=dc.id
+                    LEFT JOIN DirProdGRP as dg on dm.id_grp=dg.id
+                    WHERE div.id<>"+Division.id+
+                    " ORDER BY div.mm, div.yy";
+
+                m_sqlCmd = new SQLiteCommand(query, dblite);
+                m_sqlCmd.Connection = dblite;
+                sqlReader = m_sqlCmd.ExecuteReader();
+
+                while (await sqlReader.ReadAsync())
+                {
+                    ListViewItem item = new ListViewItem(new string[] {
+                    Convert.ToString(sqlReader["id"]),
+                    Convert.ToString(sqlReader["mm"]),
+                    Convert.ToString(sqlReader["yy"]),
+                    Convert.ToString(sqlReader["mName"]),
+                    Convert.ToString(sqlReader["pName"])+" "+Convert.ToString(sqlReader["category"])+" "+Convert.ToString(sqlReader["GRP"]),
+                    Convert.ToString(sqlReader["work_time"]),
+                    Convert.ToString(sqlReader["cost"])
+                    });
+                    item.Font = new Font(lvDivisionsToCopy.Font, FontStyle.Regular);
+                    lvDivisionsToCopy.Items.Add(item);
+                }
+
+                Division.autoResizeColumns(lvDivisionsToCopy);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 5.09.07", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                if (sqlReader != null && !sqlReader.IsClosed)
+                    sqlReader.Close();
+            }
+        }
+
+        private async void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                insertDirCommand = new SQLiteCommand(@"INSERT INTO inDivision (id_division,id_oper,rank,MatRate, NVRforOper, workers_cnt, mm,yy) 
+                                                 SELECT @id_div,id_oper, rank, MatRate, NVRforOper, workers_cnt,01,2024 FROM inDivision where id_division=@copied_id",dblite);
+                insertDirCommand.Parameters.AddWithValue("id_div", Division.id);
+                insertDirCommand.Parameters.AddWithValue("mm", Division.mm);
+                insertDirCommand.Parameters.AddWithValue("yy", Division.yy);
+                insertDirCommand.Parameters.AddWithValue("copied_id", lvDivisionsToCopy.SelectedItems[0].SubItems[0].Text);    //id копируемого разделения
+
+                await insertDirCommand.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка 8.08.08", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally 
+            {
+                this.Close();
+            }
+        }
+    }
+}
