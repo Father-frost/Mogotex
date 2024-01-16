9c31d580e43fb5b9b77735813167631e757df274
Aleksander <aleksander.kolyada@gmail.com>
2024-01-16 15:39:01 +0300

    Create separate class Division.

---
+++ WorkDivision/WorkDivision/Division.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows.Forms;
+
+namespace WorkDivision
+{
+    internal class Division
+    {
+        public static string id { get; set; }
+        public static string mm { get; set; }       //месяц
+        public static string yy { get; set; }       //год
+        public static string product { get; set; }  //изделие
+        public static string model { get; set; }    //модель
+        public static double absSumCost { get; set; }  //Суммарная расценка
+        public static double absSumNVR { get; set; }     //Суммарная норма времени
+        public static double absSumItem { get; set; }    //Суммарная стоимость
+
+
+        // авторасширение колонок listView по размеру содержимого
+        public static void autoResizeColumns(ListView lv)
+        {
+            lv.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+            ListView.ColumnHeaderCollection cc = lv.Columns;
+            for (int i = 0; i < cc.Count; i++)
+            {
+
+                int colWidth = TextRenderer.MeasureText(cc[i].Text, lv.Font).Width + 10;
+                if (i > 0)
+                {
+                    if (colWidth > cc[i].Width)
+                    {
+                        cc[i].Width = colWidth;
+                    }
+                }
+                else
+                {
+                    //Если справочник операций, отображать id
+                    if (lv.Name != "lvDirOpers")
+                    {
+                        cc[i].Width = 0;
+                    }
+                }
+            }
+        }
+    }
+}
